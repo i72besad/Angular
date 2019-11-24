3e130f940996ec4710778ad9cd568d9291060e90
i72besad <i72besad@uco.es>
2019-11-24 21:46:04 +0100

    cambios

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,35 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [
-    trigger('aniBoton', [
-      state('inactive', style({
-        backgroundImage: 'green'
-      })),
-      state('active', style({
-        backgroundImage: '#ff0000'
-      })),
-      transition('inactive => active', animate('100ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out'))
-    ])
-  ]
 })
 export class HomeComponent implements OnInit {
-
-  public state:string = "inactive";
 
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  toggleBoton(){
-    this.state = this.state === 'active' ? 'inactive' : 'active';/*esto es un if en una linea*/
   }
 
 }
